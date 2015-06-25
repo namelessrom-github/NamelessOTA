a48ddce0a14c47e1dfe68b4f7473a001f8fc9f43
Nicholas Chum <nicholaschum@users.noreply.github.com>
2015-06-25 08:55:55 -0400

    Add final working copy of Android.mk to build

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -2,20 +2,33 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# Cardview Dir
+cardview_dir := $(LOCAL_PATH)/../../../../../../frameworks/support/v7/cardview/res
+
+LOCAL_CERTIFICATE       := platform
+LOCAL_MODULE_TAGS       := optional
+LOCAL_PACKAGE_NAME      := BlissOTA
+LOCAL_PRIVILEGED_MODULE := true
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
 	RootTools \
 	android-support-v4 \
-	android-support-v7-cardview \
+	android-support-v7-cardview
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PROGUARD_ENABLED := disabled
+
+LOCAL_AAPT_FLAGS := \
+	--auto-add-overlay \
+	--extra-packages android.support.v7.cardview \
+	--extra-packages RootTools
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(cardview_dir)
 LOCAL_SRC_FILES    := $(call all-java-files-under,java)
-
-# LOCAL_PROGUARD_FLAG_FILES := ../../proguard-rules.pro
-
-LOCAL_PACKAGE_NAME      := BlissOTA
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_MODULE_TAGS       := optional
 
 include $(BUILD_PACKAGE)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libs/RootTools.jar
+
+include $(BUILD_MULTI_PREBUILT)
