84b22056b53a73b562d84cc8fe6864d3226d5079
Nicholas Chum <nicholaschum@gmail.com>
2015-07-13 03:03:44 -0400

    Add WIP Languages into BlissOTA
    These languages are still a WIP, credits all go to
    
    - gbyx
    - taranbeer
    - rahulraimarine
    - namanbhalla1998
    - goodedward19
    - qwiko
    
    To continue translating, please fork this repo and pull request it.
    Thanks!
    
    This commit comes with ALL WIP languages together:
    - Hindi
    - Hebrew
    - Swedish
    - Chinese (Traditional)

--- app/src/main/java/in/uncod/android/bypass/ReverseSpannableStringBuilder.java
+++ app/src/main/java/in/uncod/android/bypass/ReverseSpannableStringBuilder.java
@@ -4,11 +4,11 @@ import android.text.SpannableStringBuilder;
 
 /**
  * Exactly the same as SpannableStringBuilder, but it returns its spans in reverse.
- * <p>
+ * <p/>
  * What effect does this have? Well, if you're building up a Spannable recursively (as we
  * are doing in Bypass) then returning the spans in reverse order has the correct effect
  * in some corner cases regarding leading spans.
- * <p>
+ * <p/>
  * Example:
  * Suppose we have a BLOCK_QUOTE with a LIST inside of it. Both of them have leading spans, but the LIST
  * span is set first. As a result, the QuoteSpan for the BLOCK_QUOTE is actually indented by the LIST's span!
